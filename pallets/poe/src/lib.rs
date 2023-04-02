09cfed6480ff6d630084d0a9a8dbb2b1bb5e5af4
Shunji Zhan <shunhahaha@live.com>
2023-04-02 19:12:45 +0800

    add transfer claim

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -40,6 +40,7 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		ClaimCreated(T::AccountId, Vec<u8>),
 		ClaimRevoked(T::AccountId, Vec<u8>),
+		ClaimTransferred(T::AccountId, T::AccountId, Vec<u8>),
 	}
 
 	// Errors inform users that something went wrong.
@@ -56,8 +57,7 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		/// create a claim
 		#[pallet::weight(0)]
 		pub fn create_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResultWithPostInfo {
 			// Check that the extrinsic was signed and get the signer.
@@ -81,6 +81,7 @@ pub mod pallet {
 			Ok(().into())
 		}
 
+		/// revoke a claim
 		#[pallet::weight(0)]
 		pub fn revoke_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResultWithPostInfo {
 			let sender = ensure_signed(origin)?;
@@ -95,6 +96,29 @@ pub mod pallet {
 
 			// Emit an event.
 			Self::deposit_event(Event::ClaimRevoked(sender, claim));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(().into())
+		}
+
+		/// transfer a claim
+		#[pallet::weight(0)]
+		pub fn transfer_claim(origin: OriginFor<T>, to: T::AccountId, claim: Vec<u8>) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			// Update storage.
+			let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone())
+				.map_err(|_| Error::<T>::ClaimTooLong)?;
+			let (owner, _) = Proofs::<T>::get(&bounded_claim).ok_or(Error::<T>::ClaimNotExist)?;
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::remove(&bounded_claim);
+			Proofs::<T>::insert(
+				&bounded_claim,
+				(to.clone(), frame_system::Pallet::<T>::block_number()),
+			);
+
+			// Emit an event.
+			Self::deposit_event(Event::ClaimTransferred(sender, to, claim));
 			// Return a successful DispatchResultWithPostInfo
 			Ok(().into())
 		}
